c428601ff927573908cb7708e9649044e4381df7
motiurdev <motiurrahmanmisu@gmail.com>
2022-01-01 20:44:10 +0600

    login and register page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import TopBar from "./components/TopBar/TopBar";
 import Single from "./pages/Single/Single";
 import Write from "./pages/Write/Write";
 import Settings from "./pages/Settings/Settings";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
 
 
 function App() {
   return (
     <>
       <TopBar></TopBar>
-      <Settings></Settings>
+      <Register></Register>
     </>
   );
 }
