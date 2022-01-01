1af9d1274a4d2341d9037dc3d21ef0a67819e881
motiurdev <motiurrahmanmisu@gmail.com>
2022-01-01 20:29:59 +0600

    user account setting page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import Home from "./pages/Home/Home";
 import TopBar from "./components/TopBar/TopBar";
 import Single from "./pages/Single/Single";
 import Write from "./pages/Write/Write";
+import Settings from "./pages/Settings/Settings";
 
 
 function App() {
   return (
     <>
       <TopBar></TopBar>
-      <Write></Write>
+      <Settings></Settings>
     </>
   );
 }
